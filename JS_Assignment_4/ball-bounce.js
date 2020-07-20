4ae913299e4b77011282b7928327b83e90047115
Kirtee011 <kirteemaharjan123@gmail.com>
2020-07-20 16:56:11 +0545

    move, rename files & update ball-bounce.js

--- JS_Assignment_4/ball-bounce.js
+++ JS_Assignment_4/ball-bounce.js
@@ -13,6 +13,7 @@ var btn = document.getElementById('btn');
 var container = document.getElementById('frame');
 var interval;
 var direction = 1;
+var i = 0;
 
 //------ getting heoight of window and frame ------
 const frameHeight = container.clientHeight;
@@ -23,22 +24,32 @@ const margin = window.innerHeight - frameHeight;
 btn.onclick = function() {
 	var newCircle = document.createElement('div');
 	createBall(newCircle);
+	newCircle.classList.add('pointer');
+	newCircle.id = i;
 	var ballHeight = parseInt(newCircle.style.height);
 	var ballWidth = parseInt(newCircle.style.width);
-	console.log(ballHeight, ballWidth);
-	newCircle.style.top = Math.abs(Math.random() * frameHeight - ballHeight) + 'px';
-	newCircle.style.left = Math.abs(Math.random() * frameWidth - ballWidth) + 'px';
-	container.appendChild(newCircle);
+
+	newCircle.style.top = Math.abs(Math.random() * (frameHeight - ballHeight)) + 'px';
+	newCircle.style.left = Math.abs(Math.random() * (frameWidth - ballWidth)) + 'px';
 
 	newCircle.addEventListener('click', function() {
-		interval = setInterval(function() {
-			var currentTop = newCircle.style.top;
-			var nextTop = parseInt(currentTop) + 10 * direction;
-			console.log(nextTop, frameHeight);
-			newCircle.style.top = nextTop + 'px';
-			if (nextTop >= frameHeight - ballHeight || nextTop <= 0) {
-				direction *= -1;
-			}
-		}, 1000 / 60);
+		setInterval(
+			(function() {
+				var ball = newCircle;
+				var dir = direction;
+				return function() {
+					var currentTop = ball.style.top;
+					var nextTop = parseInt(currentTop) + 15 * dir;
+					ball.style.top = nextTop + 'px';
+					if (nextTop >= frameHeight - ballHeight || nextTop <= 0) {
+						dir *= -1;
+					}
+				};
+			})(),
+			1000 / 60
+		);
 	});
+
+	container.appendChild(newCircle);
+	i++;
 };
