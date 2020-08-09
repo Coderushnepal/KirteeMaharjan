1881a41c38bb0b7f7fccf3b4dc8bd5c5e6ce5ab3
Kirtee011 <kirteemaharjan123@gmail.com>
2020-08-09 20:23:00 +0545

    add features: till beer card

---
+++ React/Assignment/kirtee-app/src/components/favourites/Favourites.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from '../common/header';
+class Favourites extends Component {
+	render() {
+		return (
+			<div className="wrapper">
+				<Header />
+				<h3>Inside Fav</h3>
+			</div>
+		);
+	}
+}
--- React/Assignment/kirtee-app/src/components/main/beers/BeerGrid.js
+++ React/Assignment/kirtee-app/src/components/main/beers/BeerGrid.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 
-import Header from '../../common/header';
+import { dummyBeersData } from '../../../constants/dummyData';
+import { Header, Spinner } from '../../common';
+import Beer from './Beer';
 
 class BeerGrid extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isLoading: true,
+			beers: []
+		};
+	}
+
+	scrollParentRef = null;
+
+	fetchBeers = async () => {
+		setTimeout(() => {
+			this.setState({
+				beers: dummyBeersData,
+				isLoading: false
+			});
+		}, 2000);
+	};
+	componentDidMount() {
+		this.fetchBeers();
+	}
+
 	render() {
+		const isLoading = this.state.isLoading;
 		return (
 			<div>
 				<Header />
+				{isLoading ? (
+					<Spinner />
+				) : (
+					<main>
+						<div className="container" ref={(r) => (this.scrolParentRef = r)} />
+						{this.state.beers.map((beer) => <Beer key={beer.id} info={beer} />)}
+					</main>
+				)}
 			</div>
 		);
 	}
