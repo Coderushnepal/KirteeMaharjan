6233cd93f16510bf612c8ce6ee12143bc53899f0
Kirtee011 <kirteemaharjan123@gmail.com>
2020-08-21 02:08:27 +0545

    Updates

---
+++ precache-manifest.6940ef3c9c04439118deddf4d385dc75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc78b94b0471d08d0913bc26a2d81b70",
+    "url": "/KirteeMaharjan/React/Assignment/index.html"
+  },
+  {
+    "revision": "50ad26588f615c61d137",
+    "url": "/KirteeMaharjan/React/Assignment/static/css/2.aa7e5f6d.chunk.css"
+  },
+  {
+    "revision": "ae25c1d4a710e166bb4e",
+    "url": "/KirteeMaharjan/React/Assignment/static/css/main.eb3c95b6.chunk.css"
+  },
+  {
+    "revision": "50ad26588f615c61d137",
+    "url": "/KirteeMaharjan/React/Assignment/static/js/2.01bdbca5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/KirteeMaharjan/React/Assignment/static/js/2.01bdbca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae25c1d4a710e166bb4e",
+    "url": "/KirteeMaharjan/React/Assignment/static/js/main.23ef6a1e.chunk.js"
+  },
+  {
+    "revision": "8e20db46f6ccbba6023b",
+    "url": "/KirteeMaharjan/React/Assignment/static/js/runtime-main.4116eda1.js"
+  },
+  {
+    "revision": "7ecfe9e37e3a125f9b99d7c57c27157c",
+    "url": "/KirteeMaharjan/React/Assignment/static/media/12.7ecfe9e3.png"
+  }
+]);
