f1be03f6604d178cc2a2332d6c1f6dbfe299b164
Kirtee011 <kirteemaharjan123@gmail.com>
2020-09-02 12:41:34 +0545

    rename user-api

--- Node/users-api/src/models/UserPhoneNumber.js
+++ Node/users-api/src/models/UserPhoneNumber.js
@@ -6,8 +6,6 @@ const table = "users_phone_numbers";
 
 export async function add(params) {
   const data = snakeize(params);
-  console.log("**********", data);
   const [result] = await connection.batchInsert(table, data);
-  console.log(result);
   return camelize(result);
 }
