d57d7b925cc6b8b38071c52bede862b86e39bad2
Kirtee011 <kirteemaharjan123@gmail.com>
2020-09-08 16:31:01 +0545

    -add constants for endpoints -add scripts in package.json

--- Node/users-api/src/constants/endpoints.js
+++ Node/users-api/src/constants/endpoints.js
@@ -1,6 +1,10 @@
-export const GET_USERS = '/users';
-export const LOGIN = '/users/login';
+//users api
 export const CREATE_USER = '/users';
-export const DELETE_USER = '/users/:userId';
-export const UPDATE_USER = '/users/:userId';
-export const GET_USERS_BY_ID = '/users/:userId';
+export const LOGIN = '/users/login';
+
+//todos api
+export const GET_ALL_TODOS = '/users/:userId/todos';
+export const GET_TODO_BY_ID = '/users/:userId/todos/:todoId';
+export const ADD_TODO = '/users/:userId/todos';
+export const REMOVE_TODO = '/users/:userId/todos/:todoId';
+export const UPDATE_TODO = '/users/:userId/todos/:todoId';
